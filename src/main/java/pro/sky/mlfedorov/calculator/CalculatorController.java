fcbe38510f435b54fca3a6f4900e486872736348
maksimfedorov <fedorov-max89@mail.ru>
2024-08-12 23:14:43 +0300

    lesson134111, methods created

---
+++ src/main/java/pro/sky/mlfedorov/calculator/CalculatorController.java
@@ -0,0 +1,56 @@
+package pro.sky.mlfedorov.calculator;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CalculatorController {
+    private final CalculatorService calculatorService;
+
+    public CalculatorController(CalculatorService calculatorService) {
+        this.calculatorService = calculatorService;
+    }
+
+    @GetMapping
+    public String helloRoot() {
+        return calculatorService.hello();
+    }
+
+    @GetMapping(path = "/calculator")
+    public String hello() {
+        return calculatorService.hello();
+    }
+
+    @GetMapping(path = "/calculator/plus")
+    public String plus(
+            @RequestParam(value = "num1", required = false) Integer num1,
+            @RequestParam(value = "num2", required = false) Integer num2
+    ) {
+        return "Ответ: " + calculatorService.plus(num1, num2);
+    }
+
+    @GetMapping(path = "/calculator/minus")
+    public String minus(
+            @RequestParam(value = "num1", required = false) Integer num1,
+            @RequestParam(value = "num2", required = false) Integer num2
+    ) {
+        return "Ответ: " + calculatorService.minus(num1, num2);
+    }
+
+    @GetMapping(path = "/calculator/multiply")
+    public String multiply(
+            @RequestParam(value = "num1", required = false) Integer num1,
+            @RequestParam(value = "num2", required = false) Integer num2
+    ) {
+        return "Ответ: " + calculatorService.multiply(num1, num2);
+    }
+
+    @GetMapping(path = "/calculator/divide")
+    public String divide(
+            @RequestParam(value = "num1", required = false) Integer num1,
+            @RequestParam(value = "num2", required = false) Integer num2
+    ) {
+        return "Ответ: " + calculatorService.divide(num1, num2);
+    }
+}
---
+++ src/main/java/pro/sky/mlfedorov/calculator/CalculatorService.java
@@ -0,0 +1,13 @@
+package pro.sky.mlfedorov.calculator;
+
+public interface CalculatorService {
+    String hello();
+
+    int plus(Integer num1, Integer num2);
+
+    int minus(Integer num1, Integer num2);
+
+    int multiply(Integer num1, Integer num2);
+
+    double divide(Integer num1, Integer num2);
+}
