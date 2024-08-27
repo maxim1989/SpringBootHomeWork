ddc79e085164a6fa801889c7f2b5ebc4b8fcdeeb
maksimfedorov <fedorov-max89@mail.ru>
2024-08-27 17:43:05 +0300

    fix

---
+++ src/main/java/pro/sky/mlfedorov/sandbox/SandboxApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.mlfedorov.sandbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SandboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SandboxApplication.class, args);
+	}
+
+}
